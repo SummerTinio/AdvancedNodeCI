60b2c1c6c36f9332eee3381874c15e2000f010b6
SummerTinio <79723484+SummerTinio@users.noreply.github.com>
2021-06-05 22:57:38 +0800

    cache.js finally working

--- services/cache.js
+++ services/cache.js
@@ -4,8 +4,8 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get  = util.promisify(client.get); //returns promisified copy of original fxn
- 
+client.get = util.promisify(client.get); //returns promisified copy of original fxn
+
 //get reference to existing .exec() fxn from mongoose lib
 const exec = mongoose.Query.prototype.exec;
 //modified .exec fxn
@@ -14,19 +14,27 @@ const exec = mongoose.Query.prototype.exec;
 mongoose.Query.prototype.exec = async function () {
     // generating new, consistent Cache Key
     const cacheKey = JSON.stringify(Object.assign({}, this.getQuery(), { collection: this.mongooseCollection.name }));
-    
     // check if value for that key exists in Redis 
     const existingCacheValue = await client.get(cacheKey);
+
     if (existingCacheValue) {
-        console.log('existing cache value is:', existingCacheValue);
-        // returns that value (document) if it does
-        return JSON.parse(existingCacheValue);
+        console.log('skoom');
+        console.log('inside the if block, this=', this);
+
+         // parsing so V8 understands JSON data we got from Redis
+         const doc = JSON.parse(existingCacheValue);
+
+        // checks if Redis data came in an array or obj
+        // returns the appropriate document, for proper hydration of [] or {}
+        return Array.isArray(doc) 
+        ? doc.map(obj => new this.model(obj)) 
+        : this.model(doc);
     }
 
     // if not yet cached, gets that value from MongoDB
     const freshNewCacheValue = await exec.apply(this, arguments);
     // caches that value (document) in Redis
-    console.log('newly cached value is', freshNewCacheValue);
+    console.log('newly cached value is', JSON.stringify(freshNewCacheValue));
     client.set(cacheKey, JSON.stringify(freshNewCacheValue));
     // returns the already-cached document
     return freshNewCacheValue;
