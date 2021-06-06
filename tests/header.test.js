174192ae9eca667047c5302d2b685f1717fce229
SummerTinio <79723484+SummerTinio@users.noreply.github.com>
2021-06-07 05:52:55 +0800

    move all reusable test fxns to ProxiedPage

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,18 +1,24 @@
-const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
 
-let browser, page;
+// wraps up everything we need from Puppeteer -- so no need for puppeteer anymore
+// namely, .launch, .newPage, .close
+const ProxiedPage = require('./helpers/ProxiedPage');
+
+let page;
 
 beforeEach(async () => {
-    browser = await puppeteer.launch({ headless: false });
-    page = await browser.newPage(); 
+    page = await ProxiedPage.build();
     await page.goto('localhost:3000');
 })
 
 afterEach(async () => {
-    await browser.close();
+    page.close();
 })
 
 it('renders header with `Blogster`', async () => {
+    // .waitFor that specific DOM element to be rendered completely before asserting
+    await page.waitFor('a.brand-logo');
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
     expect(text).toEqual('Blogster');
 })
@@ -24,34 +30,8 @@ it('starts OAuth flow after being clicked', async () => {
 })
 
 it('shows logout button when signed in', async () => {
-    // MongoDB id for particular user -- req.user.id
-    const id = '60ba77090c0eb6521cfb07bc'; 
-    
-    const Buffer = require('safe-buffer').Buffer;
-    // fake sessionObject -- looks like a utf-8 parsed session=longWeirdString
-    const sessionObject = {
-        passport: {
-            user: id
-        }
-    };
-    //stringifying that sessionObject and translating it into base64 via toString
-    const sessionString = Buffer.from(
-        JSON.stringify(sessionObject))
-        .toString('base64');
-
-    //provide keygrip cookieSigningSecret and sessionString .. to get session.sig
-    const Keygrip = require('keygrip');
-    const keys = require('../config/keys');
-    const keygrip = new Keygrip([keys.cookieKey]);
-    const sig = keygrip.sign(`session=${sessionString}`); //note: tested sessionString and sig = correct!
-
-    await page.setCookie({ name: 'session', value: sessionString });
-    await page.setCookie({ name: 'session.sig', value: sig });
-    await page.goto('localhost:3000');
- 
-    // gotta .waitFor that anchor tag to  be rendered before asserting!
-    await page.waitFor('a[href="/auth/logout"]');
-
+    // login using fake session and session.sig
+    await page.login();
     // get a DOM node, assert
     const text = await page.$eval('.right a[href="/auth/logout"]', el => el.innerHTML);
     expect(text).toEqual('Logout');
