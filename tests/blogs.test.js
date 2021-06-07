90877224655543dd031ff8422152ba512e3803bc
SummerTinio <79723484+SummerTinio@users.noreply.github.com>
2021-06-07 08:10:51 +0800

    add nested tests for other paths of interaction

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -11,8 +11,31 @@ afterEach(async () => {
     await page.close();
 })
 
-it('displays blog creation form after logging in', async () => {
-    //page.login() does NOT redirect to /blogs endpoint. -- leaves puppeteer at localhost:3000/
-    await page.login();
+describe('When logged in,', async () => {
+    beforeEach(async () => {
+        await page.login();
+        await page.waitFor('a.btn-floating');
+        await page.click('a.btn-floating');
+    })
 
+    it('displays blog creation form', async () => {
+        const label = await page.getInnerHTML('form label')
+    
+        // assertion must be specific to this form -- so get a label that only exists in this form
+        expect(label).toEqual('Blog Title');
+    })
+
+    describe('and using invalid inputs (no input),', async () => {
+        beforeEach(async () => {
+            await page.waitFor('form button');
+            await page.click('form button');
+        })
+
+        test('the form shows 2 error messages', async () => {
+            const blogTitleError = await page.getInnerHTML('.title .red-text');
+            const contentError = await page.getInnerHTML('.content .red-text');
+            expect(blogTitleError).toEqual('You must provide a value');
+            expect(contentError).toEqual('You must provide a value');
+        })
+    })
 })
