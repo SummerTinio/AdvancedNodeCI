ef6956fcaf57e510dd47e05d81f094d5bca5ef2e
SummerTinio <79723484+SummerTinio@users.noreply.github.com>
2021-06-08 04:27:01 +0800

    add semicolons

--- tests/header.test.js
+++ tests/header.test.js
@@ -10,24 +10,24 @@ let page;
 beforeEach(async () => {
     page = await ProxiedPage.build();
     await page.goto('localhost:3000');
-})
+});
 
 afterEach(async () => {
     await page.close();
-})
+});
 
 it('renders header with `Blogster`', async () => {
     // .waitFor that specific DOM element to be rendered completely before asserting
     await page.waitFor('a.brand-logo');
     const text = await page.getInnerHTML('a.brand-logo');
     expect(text).toEqual('Blogster');
-})
+});
 
 it('starts OAuth flow after being clicked', async () => {
     await page.click('.right a');
     const url = await page.url();
     expect(url).toMatch(/accounts\.google\.com/);
-})
+});
 
 it('shows logout button when signed in', async () => {
     // login using fake session and session.sig
@@ -35,4 +35,4 @@ it('shows logout button when signed in', async () => {
     // get a DOM node, assert
     const text = await page.getInnerHTML('.right a[href="/auth/logout"]');
     expect(text).toEqual('Logout');
-})
+});
