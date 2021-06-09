154c09c579a1b38d8db09279c643d17c543ddd6e
SummerTinio <79723484+SummerTinio@users.noreply.github.com>
2021-06-09 21:07:20 +0800

    setup headless browser for ci

--- tests/helpers/ProxiedPage.js
+++ tests/helpers/ProxiedPage.js
@@ -6,21 +6,22 @@ const userFactory = require('../factories/userFactory');
 // Static methods === accessible from ProxiedPage.staticMethod()
 // class instance methods === accessible from proxiedPageInstance.instanceMethod()
 class ProxiedPage {
-    // calling CustomPage.build() 
+    // calling CustomPage.build()
     //launches a virtual browser, opens a tab on it,
     // and returns a Proxy object -- from w/c u can directly access methods on
     // customPage, page, & browser, ALL FROM THE SAME PLACE
     static async build() { // <-- creates method: CustomPage.build()
         const browser = await puppeteer.launch({
-            headless: false
-        });
+            headless: true,
+            args: ['--no-sandbox'] // so we don't need to mess around with travis's VM
+        })
 
         const page = await browser.newPage();
         const proxiedPage = new ProxiedPage(page)
-    
+
         // tells Proxy to look for property on customPage --> page --> browser in that order
         return new Proxy(proxiedPage, {
-            get: function(target, property) {
+            get: function (target, property) {
                 // change order of these fxns in order of priority
                 return proxiedPage[property] || browser[property] || page[property];
             }
@@ -43,10 +44,10 @@ class ProxiedPage {
         await this.page.setCookie({ name: 'session', value: session });
         await this.page.setCookie({ name: 'session.sig', value: sig });
         //await this.page.goto('localhost:3000');
-        
+
         //remove this line if you want to reuse this ProxiedPage elsewhere
-        await this.page.goto('localhost:3000/blogs');
-    
+        await this.page.goto('http://localhost:3000/blogs');
+
         // gotta .waitFor that anchor tag to  be rendered before asserting!
         await this.page.waitFor('a[href="/auth/logout"]');
     }
@@ -64,34 +65,34 @@ class ProxiedPage {
             //note: the entire callback below gets stringified AS IS
             //reference to apiEndpoint will NOT be evaluated as normal JS
             (_apiEndPoint, _reqBody) => {
-            return fetch(_apiEndPoint, {
-                method: 'POST',
-                credentials: 'same-origin',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(_reqBody)
-            // fetch returns a promise, so .then() to capture the resolved value
-            }).then(res => res.json());
-            // to allow apiEndpoint to get evaluated BEFORE callback is stringified
+                return fetch(_apiEndPoint, {
+                    method: 'POST',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(_reqBody)
+                    // fetch returns a promise, so .then() to capture the resolved value
+                }).then(res => res.json());
+                // to allow apiEndpoint to get evaluated BEFORE callback is stringified
             }, apiEndPoint, reqBody
         );
     }
 
     //wrapper for page.evaluate() call --> automate a POST test request
-    get(apiEndPoint) { 
+    get(apiEndPoint) {
         return this.page.evaluate(
             //_apiEndpoint is totally different variable from apiEndpoint, just with the same value
             (_apiEndPoint) => {
-            return fetch(_apiEndPoint, {
-                method: 'GET',
-                credentials: 'same-origin',
-                headers: {
-                    'Content-Type': 'application/json'
-                } //note: GET request can NOT have a body
-            // fetch returns a promise, so .then() to capture the resolved value
-            }).then(res => res.json());
-            // arg to pass into page.evaluate() callback
+                return fetch(_apiEndPoint, {
+                    method: 'GET',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    } //note: GET request can NOT have a body
+                    // fetch returns a promise, so .then() to capture the resolved value
+                }).then(res => res.json());
+                // arg to pass into page.evaluate() callback
             }, apiEndPoint
         );
     }
