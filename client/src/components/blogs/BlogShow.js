dc2cfca9c9729988481213d5d9014e15862cf7e2
SummerTinio <79723484+SummerTinio@users.noreply.github.com>
2021-06-11 06:38:12 +0800

    refactor BlogShow to display image

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -3,10 +3,22 @@ import { connect } from 'react-redux';
 import { fetchBlog } from '../../actions';
 
 class BlogShow extends Component {
+  constructor(props) {
+    super(props);
+    this.renderImage = this.renderImage.bind(this);
+  }
   componentDidMount() {
     this.props.fetchBlog(this.props.match.params._id);
   }
 
+  renderImage() {
+    //note: this.props.blog.imageUrl refers to the fileName!
+    if (this.props.blog.fileName !== null) {
+      const domain = 'https://evergreen-s3-blog-image-store.s3.ap-northeast-1.amazonaws.com/';
+      return <img src={`${domain}${this.props.blog.fileName}`} alt={this.props.blog.title}/>
+    }
+  }
+  
   render() {
     if (!this.props.blog) {
       return '';
@@ -14,10 +26,11 @@ class BlogShow extends Component {
 
     const { title, content } = this.props.blog;
 
-    return (
+    return ( 
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
