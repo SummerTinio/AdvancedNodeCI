850cc6dd7319a6eec9a509fcc12bb3e5aca98728
SummerTinio <79723484+SummerTinio@users.noreply.github.com>
2021-06-11 00:47:33 +0800

    name variables descriptively

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -9,22 +9,23 @@ const s3 = new AWS.S3({
     secretAccessKey: keys.secretAccessKey
 })
 
-const upload = function routesUsedToUploadToS3 (app) {
+const upload = function routesForS3Upload (app) {
     app.get('/api/upload', requireLogin, async (req, res) => {
         // use slash in key to enforce "structure" through file names -- even if bucket is just a flat collection
         const extension = 'jpeg';
         // key === File Name for uploaded image
-        const key = `${req.user.id}/${uuid()}.${extension}`;
+        const fileName = `${req.user.id}/${uuid()}.${extension}`;
 
         // s3params === BCK!
         const s3params = {
             Bucket: 'evergreen-s3-blog-image-store',
-            ContentType: 'jpeg',
-            Key: key
+            ContentType: 'image/*',
+            Key: fileName
         };
-
-        s3.getSignedUrl('putObject', s3params, async (err, url) => {
-            res.send({ key, url });
+        // the actual request for an AWS preSignedUrl.
+        s3.getSignedUrl('putObject', s3params, async (err, preSignedUrl) => {
+            // object returned by axios.get('/api/upload', ...) in actions/index.js --
+            res.send({ fileName, preSignedUrl });
         });
     });
 
